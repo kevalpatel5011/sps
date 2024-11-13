6f23110f33f179d7b0c85ce9aa1d101608e6afbf
kevalpatel5011 <kevalpatel5011@gmail.com>
2024-11-12 23:02:03 -0500

    2nd commit

--- game.py
+++ game.py
@@ -1,5 +1,3 @@
 def main():
-    print("Welcome to Rock Paper Scissors!")
-
-if _name_ == "_main_":
-    main()
+      print("Welcome to Rock Paper Scissors!")
+      print("Rules: Rock beats scissors, scissors beats paper, paper beats rock.")
